e5ae725a8d3e37c21c287abb30d323efc16986dc
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-03-02 07:11:00 +0000

    Add script.js file for Currency Stack Project

---
+++ currency-stack/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("mousemove",effect);
+const layers = document.querySelectorAll(".layer");
+function effect(e){
+    layers.forEach((layer)=>{
+        const movement = layer.getAttribute("data-speed");
+        let x = (window.innerWidth - e.pageX * movement) / 250;
+        let y = (window.innerWidth - e.pageX * movement) / 250;
+
+        layer.style.transform = `translate(${-x}px) translateY(${y}px)`;
+
+    })
+}
